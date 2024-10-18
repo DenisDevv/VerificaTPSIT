8f0d37ea6d1723043f3361712882526130988759
DenisDevv <karajedenis@gmail.com>
2024-10-18 10:56:31 +0200

    Sistemato il l'output del terzo thread che si fermava all'ultima operazione non dando il risultato finale

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        VerificaThread t1 = new VerificaThread(0);
-        VerificaThread t2 = new VerificaThread(0);
+        VerificaThread t1 = new VerificaThread(0, "Thread 1");
+        VerificaThread t2 = new VerificaThread(0, "Thread 2");
         VerificaThread lettore = new VerificaThread(1);
         t1.start();
         t2.start();
--- src/VerificaThread.java
+++ src/VerificaThread.java
@@ -1,14 +1,19 @@
+import java.util.Arrays;
+
 public class VerificaThread extends Thread {
     private static int[] arrayDiNumeri = new int[100];
     private int ruolo;
+    private String nome;
     private static Integer index = 0;
     public VerificaThread(int ruolo) {
-        if (arrayDiNumeri[1] == 0) {
             for (int i = 0; i < arrayDiNumeri.length; i++) {
                 arrayDiNumeri[i] = i + 1;
             }
-        }
         this.ruolo = ruolo;
+    }
+    public VerificaThread(int ruolo, String nome) {
+        this.ruolo = ruolo;
+        this.nome = nome;
     }
     public void run() {
         if (ruolo == 0) {
@@ -21,6 +26,7 @@ public class VerificaThread extends Thread {
             synchronized (index) {
                 if (arrayDiNumeri[index] == index + 1) {
                     arrayDiNumeri[index] = arrayDiNumeri[index] + 1;
+                    System.out.println(nome + " Sta modificando il record" );
                     try {
                         Thread.sleep(2000);
                     } catch (InterruptedException e) {
@@ -28,25 +34,25 @@ public class VerificaThread extends Thread {
                     }
                 } else {
                     index++;
-                    System.out.println("record già modificato");
+                    System.out.println("record già modificato, incremento l'indice " + nome);
                 }
 
             }
             }
         } else {
-                int i = 0;
-                while (index < arrayDiNumeri.length) {
+                while (index <= arrayDiNumeri.length) {
                     synchronized (index) {
-                        System.out.println("Valore: " + arrayDiNumeri[i] + " Indice: " + i);
-                        i++;
+                        System.out.println(Arrays.toString(arrayDiNumeri));
                     }
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+                    if (index == arrayDiNumeri.length-1) {
+                        break;
+                    }
                 }
-
         }
     }
 }
